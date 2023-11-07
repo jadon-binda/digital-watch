9e72b9a0c30140d152751fd2bda5a06d001200f4
JB <jadon.binda@gmail.com>
2023-11-07 12:59:53 -0300

    feat: create buttons to start and stop the watch

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,9 @@
 const hoursEl = document.querySelector('.hours');
 const minutesEl = document.querySelector('.minutes');
 const secondsEl = document.querySelector('.seconds');
+const btnStopWatch = document.querySelector('.stop-watch');
+const btnStartWatch = document.querySelector('.start-watch');
+let watch;
 
 function updateWatch(hours, minutes, seconds) {
   const now = new Date();
@@ -9,4 +12,13 @@ function updateWatch(hours, minutes, seconds) {
   seconds.textContent = now.getSeconds().toString().padStart(2, '0');
 }
 
-const watch = setInterval(() => updateWatch(hoursEl, minutesEl, secondsEl), 1000);
+function startWatch() {
+  watch = setInterval(() => updateWatch(hoursEl, minutesEl, secondsEl), 1000);
+}
+
+function stopWatch() {
+  clearInterval(watch);
+}
+
+btnStartWatch.addEventListener('click', () => startWatch());
+btnStopWatch.addEventListener('click', () => stopWatch());
